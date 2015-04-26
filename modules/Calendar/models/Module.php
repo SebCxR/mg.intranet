d140cc605d66ceea66eef7299ea0e47e1030e603
edvariables <admin@edvariables.net>
2015-04-26 19:19:39 +0200

    getMGChauffeursForCalendar : remove AND vtiger_users.status = 'Active'...

--- modules/Calendar/models/Module.php
+++ modules/Calendar/models/Module.php
@@ -371,7 +371,7 @@ class Calendar_Module_Model extends Vtiger_Module_Model {
 			JOIN vtiger_crmentity
 				ON vtiger_mgchauffeurs.mgchauffeursid = vtiger_crmentity.crmid
 			WHERE vtiger_crmentity.deleted = 0
-			"./* AND vtiger_users.status = 'Active' */."
+			/* AND vtiger_users.status = 'Active' */
 			AND vtiger_mgchauffeurs.actif = 1
 			";
 		$result = $db->query($query);
